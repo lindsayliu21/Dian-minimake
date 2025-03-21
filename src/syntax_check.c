74b61e19a0c5f4d98c3d202c84f0321513995701
lindsayliu21 <17338767697@163.com>
2025-03-21 22:45:50 +0800

    Update syntax_check.c

--- src/syntax_check.c
+++ src/syntax_check.c
@@ -2,20 +2,26 @@
 int in_rule = 0;
 
 void check_syntax(const char *line,int line_num){
+     if(in_rule&&line[0]=='\t'){
+        printf("Line%d: Command found before rule\n",line_num);
+      exit(1);
+    }else if(in_rule){
+        in_rule=0;
+    }
     //检查目标行是否有冒号
-    if(!in_rule){
         char *colon=strstr(line,":");
         if(colon==NULL){
-        if(line[0]=='\t') 
-        printf("Line%d: Command found before rule\n",line_num);
-        else printf("Line%d: Missing colon in target definition\n",line_num);
+            //没有冒号，且不是命令行
+        printf("Line%d: Missing colon in target definition\n",line_num);
         exit(1);
         }
-    }
     //检查命令是否以Tab开头
-    else{if(line[0]!='\t'){
+    else{
+        //目标行不能以tab开头
+        if(line[0]!='\t'){
         printf("Line%d: Command must start with Tab\n",line_num);
         exit(1);
     }
+    in_rule=1;
     }
 }
